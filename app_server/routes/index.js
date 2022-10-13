e4cab30dd8b2abad74de8cf9976644f18082c7b9
kursadburak <kursatburaktaskin@gmail.com>
2022-10-13 23:23:44 +0300

    Diğer Gönderim

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
-/* GET home page. */
-router.get('/',ctrlMain.index);
+var ctrlMekanlar = require('../controllers/mekanlar');
+var ctrlDigerleri = require('../controllers/digerleri');
+
+
+router.get('/',ctrlMekanlar.anaSayfa);
+router.get('/mekan',ctrlMekanlar.mekanBilgisi);
+router.get('/mekan/yorum/yeni',ctrlMekanlar.yorumEkle);
+router.get('/hakkinda',ctrlDigerleri.hakkinda);
 
 module.exports = router;
