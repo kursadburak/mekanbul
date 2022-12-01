4bc3adb11e0071287df5298f29aefadb1109953e
kursadburak <kursatburaktaskin@gmail.com>
2022-12-01 21:28:41 +0300

    odev7

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,7 +1,7 @@
 const axios=require("axios");
 var apiSecenekleri={
-    //sunucu:"http://localhost:3000",
-    sunucu:"https://mekanbul.kursadburak.repl.co/",
+    sunucu:"http://localhost:3000",
+    //sunucu:"https://mekanbul.kursadburak.repl.co/",
     apiYolu:"/api/mekanlar/"
 }
 var mesafeyiFormatla=function(mesafe){
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,10 +3,8 @@ var router = express.Router();
 var ctrlMekanlar = require('../controllers/mekanlar');
 var ctrlDigerleri = require('../controllers/digerleri');
 
-
 router.get('/',ctrlMekanlar.anaSayfa);
-router.get('/mekan/:mekanid',ctrlMekanlar.mekanBilgisi);
-router.get('/mekan/yorum/yeni',ctrlMekanlar.YorumEkle);
+router.get('/mekan/:mekanid', ctrlMekanlar.mekanBilgisi);
+router.get('/mekan/yorum/yeni',ctrlMekanlar.yorumEkle);
 router.get('/hakkinda',ctrlDigerleri.hakkinda);
-
 module.exports = router;
